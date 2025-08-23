d0b1c9fbf1fdd70f99760bfe49cbe65586ef8a54
Green <39230440+oldfart95@users.noreply.github.com>
2025-08-23 14:10:56 -0400

    Update source code

--- autopost.bat
+++ autopost.bat
@@ -2,6 +2,9 @@
 rem Change directory to the script's location to ensure invoke runs correctly
 cd /d "%~dp0"
 
+echo Activating virtual environment...
+call .\.venv\Scripts\activate
+
 echo Running autopost command...
 invoke autopost
 echo.
